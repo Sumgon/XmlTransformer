5f52920a4c99e0e5da9cee5b0a0555b0627f5265
Sumgon <574368741@qq.com>
2019-11-22 18:27:22 +0800

    添加项目文件。

---
+++ XmlTransformer/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+
+namespace XmlTransformer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApplyTransform_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(xmlSelector.Path) && File.Exists(xsltSelector.Path))
+            {
+                string xmlFilePath = xmlSelector.Path;
+                string xsltPath = xsltSelector.Path;
+                XPathDocument doc = new XPathDocument(xmlFilePath);
+                XslCompiledTransform trans = new XslCompiledTransform();
+                trans.Load(xsltPath);
+                using (XmlTextWriter writer = new XmlTextWriter(htmlSelector.Path, null))
+                {
+                    trans.Transform(doc, null, writer);
+                };           
+                MessageBox.Show("Success!");
+            }
+            else
+            {
+                MessageBox.Show("Please fill the three paths and ensure xml and xslt file exist!");
+            }
+        }
+    }
+}
---
+++ XmlTransformer/PathSelector.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace XmlTransformer
+{
+    /// <summary>
+    /// PathSelector.xaml 的交互逻辑
+    /// </summary>
+    public partial class PathSelector : UserControl
+    {
+        public PathSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            var dialogResult = dialog.ShowDialog();
+            if (dialogResult.HasValue)
+            {
+                if (dialogResult.Value)
+                {
+                    this.Path = dialog.FileName;
+                }
+            }
+        }
+
+        public string Path
+        {
+            get { return (string)GetValue(PathProperty); }
+            set { SetValue(PathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Path.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PathProperty =
+            DependencyProperty.Register("Path", typeof(string), typeof(PathSelector), new PropertyMetadata(""));
+
+
+    }
+}
